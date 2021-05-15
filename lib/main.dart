7f8c7662ca43414c390c801a89ff8dcb6a647313
muhammad23779 <rasoolmuammad@gmail.com>
2021-05-15 11:43:40 -0700

    Practice with new

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+
+
+void main() {
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold
+               (body: Text("Muhammad Rasool email   rasoolmuammad@gmail.com"),
+      ),
+    );
+  }
+}
+
